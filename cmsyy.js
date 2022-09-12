38fb08ab0e565e149833f784eb36a47b89223ae1
tv98 <511457465@qq.com>
2022-09-13 02:30:41 +0800

    Add files via upload

---
+++ cmsyy.js
@@ -0,0 +1,10 @@
+document.writeln("<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement(\"script\");
+  hm.src = \"https:\/\/hm.baidu.com\/hm.js?0866b6dbcfead5d4971e7cdcdca1c66a\";
+  var s = document.getElementsByTagName(\"script\")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+<\/script>
+");
