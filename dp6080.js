5e40a5ba335e6e80f8118d65b4c49b2b47a5f022
tv98 <511457465@qq.com>
2022-09-13 02:21:10 +0800

    Add files via upload

---
+++ dp6080.js
@@ -0,0 +1,10 @@
+document.writeln("<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement(\"script\");
+  hm.src = \"https:\/\/hm.baidu.com\/hm.js?df8cd74bd1418085d823ab2da543e4ca\";
+  var s = document.getElementsByTagName(\"script\")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+<\/script>
+");
