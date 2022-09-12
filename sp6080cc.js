f4dd946a8538299ffb3e65621aee5329fe7ef09b
tv98 <511457465@qq.com>
2022-09-13 02:10:36 +0800

    Add files via upload

---
+++ sp6080cc.js
@@ -0,0 +1,10 @@
+document.writeln("<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement(\"script\");
+  hm.src = \"https:\/\/hm.baidu.com\/hm.js?2c80ae9a8c46b0d091fc74a98a9cc02a\";
+  var s = document.getElementsByTagName(\"script\")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+<\/script>
+");
